54839303254e9af9d7ede3489698d386b6aad4f6
blackwell <blackwellsmithiv@gmail.com>
2019-02-13 21:33:32 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -9,7 +9,7 @@ class Patient
   def self.all
     @@all
   end
-  def new_appointment(date, doctor)
+  def new_appointment( doctor, date)
     Appointment.new( self, doctor, date)
   end
   def appointments
